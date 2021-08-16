aaeebf26ca165a43e7a23a518ba54944fc311b77
MeHigh22 <72247500+MeHigh22@users.noreply.github.com>
2021-08-16 14:52:45 +0200

    Commit Début

---
+++ resources/views/partials/images.blade.php
@@ -0,0 +1,13 @@
+<div class="container mt-3">
+    <h1 class="my-3">All</h1>
+
+    <div class="my-5">
+        <h3>Ici sont vos images !</h3>
+
+    </div>
+
+    <div class="my-5">
+        <h3>Les fichiers</h3>
+
+    </div>
+</div>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,6 @@
+@extends("layouts.index")
+
+@section("content")
+@include("partials.images")
+<h1>Bienvenue sur notre page d'accueuil !</h1>
+@endsection
